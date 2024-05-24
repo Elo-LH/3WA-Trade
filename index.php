43171f2ea3d938d91713585ec5ecb288745d2262
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-05-24 11:47:07 +0200

    feat: cookies to set color, delete cookie

--- index.php
+++ index.php
@@ -1,44 +1,33 @@
 <?php
-
  require "connexion.php";
 
 
+ if(isset($_COOKIE['color'])){
+    $color = $_COOKIE['color'];
+ }else {
+    $color = "%00000000";
+};
+
 
     if (!array_filter($_GET)){
-     
         $template = "index.phtml";
         require "layout.phtml";
-        
     }else{
-
         if(isset($_GET['cat']) ){
             $param = $_GET['cat'];
-            if(isset($_COOKIES['color'])){
-                $color = $_COOKIES['color'];
-             }else {
-                $color = "%00000000";
-            };
             $template = "category.phtml";
             require "layout.phtml";
+        }else if(isset($_GET['cookie']) ){
+            setcookie('color'); 
+            unset($_COOKIE['color']);
+            $template = "index.phtml";
+            require "layout.phtml";
         }else{
-            echo 'Enter alternative';
-            setcookie('color', $_GET['color'], time() + 365*24*3600); 
-            print_r($_COOKIES);
-            echo $_COOKIES['color'];
-            if(isset($_COOKIES['color'])){
-                $color = $_COOKIES['color'];
-                echo 'Enter cookie';
-             }else {
-                $color = "%00000000";
-                echo 'Enter white';
-            };
+            setcookie('color', $_GET['color'], time() + 365*24*3600,  '/' ); 
+            $_COOKIE['color'] = $_GET['color'];
+            $color = $_COOKIE['color'];
             $template = "index.phtml";
             require "layout.phtml";
         }
-        
-       
-       
-
     }
-   
 ?>

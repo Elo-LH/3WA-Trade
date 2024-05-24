f9b1c9fe1a5d7cc1e0e2ce885f579659225a526d
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-05-24 10:48:48 +0200

    test cookies

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
  require "connexion.php";
-  
+
+
 
     if (!array_filter($_GET)){
      
@@ -9,10 +10,34 @@
         require "layout.phtml";
         
     }else{
-        $param = $_GET['cat'];
+
+        if(isset($_GET['cat']) ){
+            $param = $_GET['cat'];
+            if(isset($_COOKIES['color'])){
+                $color = $_COOKIES['color'];
+             }else {
+                $color = "%00000000";
+            };
+            $template = "category.phtml";
+            require "layout.phtml";
+        }else{
+            echo 'Enter alternative';
+            setcookie('color', $_GET['color'], time() + 365*24*3600); 
+            print_r($_COOKIES);
+            echo $_COOKIES['color'];
+            if(isset($_COOKIES['color'])){
+                $color = $_COOKIES['color'];
+                echo 'Enter cookie';
+             }else {
+                $color = "%00000000";
+                echo 'Enter white';
+            };
+            $template = "index.phtml";
+            require "layout.phtml";
+        }
+        
        
-        $template = "category.phtml";
-        require "layout.phtml";
+       
 
     }
    

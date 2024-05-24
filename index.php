331e2e5c8ff4e7fc62aa167861ec578a064362ba
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-05-24 11:59:42 +0200

    feat : bonus auto text color in contrast to background

--- index.php
+++ index.php
@@ -2,31 +2,50 @@
  require "connexion.php";
 
 
+ function getContrastColor(string $hexcolor): string
+ {
+    $r = hexdec(substr($hexcolor, 1, 2));
+    $g = hexdec(substr($hexcolor, 3, 2));
+    $b = hexdec(substr($hexcolor, 5, 2));
+    $yiq = (($r * 299) + ($g * 587) + ($b * 114)) / 1000;
+    return ($yiq >= 128) ? 'black' : 'white';
+ };
+   
+
  if(isset($_COOKIE['color'])){
     $color = $_COOKIE['color'];
  }else {
-    $color = "%00000000";
+    $color = "%ffffff";
 };
 
 
     if (!array_filter($_GET)){
         $template = "index.phtml";
+        $textColor = getContrastColor($color);
         require "layout.phtml";
     }else{
         if(isset($_GET['cat']) ){
             $param = $_GET['cat'];
             $template = "category.phtml";
+        $textColor = getContrastColor($color);
+
             require "layout.phtml";
         }else if(isset($_GET['cookie']) ){
             setcookie('color'); 
             unset($_COOKIE['color']);
+            $color = "%ffffff";
             $template = "index.phtml";
+        $textColor = getContrastColor($color);
+
+
             require "layout.phtml";
         }else{
             setcookie('color', $_GET['color'], time() + 365*24*3600,  '/' ); 
             $_COOKIE['color'] = $_GET['color'];
             $color = $_COOKIE['color'];
             $template = "index.phtml";
+        $textColor = getContrastColor($color);
+
             require "layout.phtml";
         }
     }
